cfdef2ebd3f2eca11e78883f23d771c2c05c233c
maker2413 <maker2413@gmail.com>
2024-12-01 06:21:43 -0600

    finished problems 23 and started problem 24

---
+++ Go/023/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	to = 28123
+)
+
+var (
+	abundantNums     [to + 1]bool
+	abundantNumsList []int
+)
+
+func checkAbundant(n int) bool {
+	sum := 1
+
+	for i := 2; i < int(math.Pow(float64(n), .5))+1; i++ {
+		if n%i == 0 {
+			j := n / i
+			sum += i
+			if i != j {
+				sum += j
+			}
+		}
+	}
+
+	if sum > n {
+		return true
+	}
+
+	return false
+}
+
+func checkSumOfAbundant(n int) bool {
+	for j := 0; j < len(abundantNumsList); j++ {
+		if abundantNumsList[j] < n {
+			if abundantNums[n-abundantNumsList[j]] {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func main() {
+	var answer int
+
+	for i := 6; i <= to; i++ {
+		abundantNums[i] = checkAbundant(i)
+		if abundantNums[i] {
+			abundantNumsList = append(abundantNumsList, i)
+		}
+	}
+
+	for i := 1; i <= to; i++ {
+		if !checkSumOfAbundant(i) {
+			answer += i
+		}
+	}
+
+	fmt.Println(checkAbundant(16))
+	fmt.Println(answer)
+}
---
+++ Go/024/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func permutations(s string) []string {
+	var str []string
+
+	return str
+}
+
+func main() {
+	var answer int
+
+	fmt.Println(answer)
+}
